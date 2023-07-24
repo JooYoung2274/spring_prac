968a6eae4ad9d1a1a234920eb10d3c6392c28ded
JooYoung2274 <jooooo2274@gmail.com>
2023-07-24 21:13:25 +0900

    테스트 코드 예시 작성 (h2 연결 테스트)

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional // test에 Transactional이 있으면 테스트 끝나고 데이터 롤백해버림
+    @Rollback(false) // Transactional에 의한 롤백이 싫으면 false로 설정
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); // findMember랑 member랑 같음. 같은 영속성 컨텍스트 안에서는 식별자가 같으면 같은 엔티티로 식별함
+    }
+}
